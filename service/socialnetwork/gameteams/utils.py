73c4d3d43469dc9b957d28ed89d74a8efc3a84d3
Alexey <alexxey2707@gmail.com>
2024-05-21 20:01:56 +0200

    gameteams: added types annotations to TeamHandle class

--- service/socialnetwork/gameteams/utils.py
+++ service/socialnetwork/gameteams/utils.py
@@ -1,37 +1,38 @@
-from .models import TeamJoinRequest
+from .models import TeamJoinRequest, Team
+from users.models import Profile
+from django.db.models.query import QuerySet
 
 class TeamHandle:
-    def __init__(self, team):
+    def __init__(self, team: Team) -> None:
         self.team = team
         
-    def get_join_request(self, user_id):
+    def get_join_request(self, user_id: int) -> TeamJoinRequest:
         join_request = TeamJoinRequest.objects.get(
             to_team=self.team, from_user=user_id
         )
         return join_request
     
-    def get_all_join_requests(self):
+    def get_all_join_requests(self) -> QuerySet[TeamJoinRequest]:
         return TeamJoinRequest.objects.filter(to_team=self.team)
 
-    def create_join_request(self, user_id):
-        join_request = TeamJoinRequest.objects.get_or_create(
+    def create_join_request(self, user_id: int) -> tuple[TeamJoinRequest, bool]:
+        join_request, created = TeamJoinRequest.objects.get_or_create(
             to_team=self.team, from_user=user_id
         )
-        return join_request
+        return (join_request, created)
     
-    def accept_join_request(self, user_profile):
+    def accept_join_request(self, user_profile: Profile) -> TeamJoinRequest:
         join_request = self.get_join_request(user_profile.user_id)
         join_request.delete()
         user_profile.team = self.team
         user_profile.save()
-        return self.team.members
+        return join_request
     
-    def remove_join_request(self, user_profile):
+    def remove_join_request(self, user_profile: Profile) -> TeamJoinRequest:
         join_request = self.get_join_request(user_profile.user_id)
         join_request.delete()
-        return self.team
+        return join_request
     
-    def remove_member(self, user_profile):
+    def remove_member(self, user_profile: Profile) -> None:
         user_profile.team = None
         user_profile.save()
-        return self.team
